fe3c45c6ead19b70d81df05d00726e3ea2de4df4
Tucker Ulrich <5263190+Juiix@users.noreply.github.com>
2022-12-28 00:22:54 -0700

    Initial code commit

---
+++ Pixl.Editor/Program.cs
@@ -0,0 +1,94 @@
+﻿using ImGuiNET;
+using System.Diagnostics;
+using Veldrid;
+using Veldrid.StartupUtilities;
+
+namespace Pixl.Editor
+{
+    public class Program
+    {
+        private static readonly RgbaFloat s_clearColor = new RgbaFloat(0, 0, 0, 1);
+
+        public static void Main(string[] args)
+        {
+            VeldridStartup.CreateWindowAndGraphicsDevice(
+                new WindowCreateInfo(50, 50, 1280, 720, WindowState.Normal, "ImGui.NET Sample Program"),
+                new GraphicsDeviceOptions(true, null, true, ResourceBindingModel.Improved, true, true),
+                out var window,
+                out var gd);
+
+            var renderer = new ImGuiRenderer(gd, gd.MainSwapchain.Framebuffer.OutputDescription, window.Width, window.Height);
+            window.Resized += () =>
+            {
+                gd.MainSwapchain.Resize((uint)window.Width, (uint)window.Height);
+                renderer.WindowResized(window.Width, window.Height);
+            };
+            var cl = gd.ResourceFactory.CreateCommandList();
+
+            // Main application loop
+
+            long timestamp = Stopwatch.GetTimestamp();
+            while (window.Exists)
+            {
+                InputSnapshot snapshot = window.PumpEvents();
+                if (!window.Exists) { break; }
+                renderer.Update(1f / 60f, snapshot); // Feed the input events to our ImGui controller, which passes them through to ImGui.
+
+                SubmitUI();
+
+                cl.Begin();
+                cl.SetFramebuffer(gd.MainSwapchain.Framebuffer);
+                cl.ClearColorTarget(0, s_clearColor);
+                renderer.Render(gd, cl);
+                cl.End();
+                gd.SubmitCommands(cl);
+                gd.SwapBuffers(gd.MainSwapchain);
+            }
+
+            // Clean up Veldrid resources
+            gd.WaitForIdle();
+            renderer.Dispose();
+            cl.Dispose();
+            gd.Dispose();
+        }
+
+        private static void SubmitUI()
+        {
+            ImGui.Begin("Another Window");
+            ImGui.Text("Hello from another window!");
+            if (ImGui.Button("Close Me"))
+            ImGui.End();
+
+            if (ImGui.TreeNode("Tabs"))
+            {
+                if (ImGui.TreeNode("Basic"))
+                {
+                    ImGuiTabBarFlags tab_bar_flags = ImGuiTabBarFlags.None;
+                    if (ImGui.BeginTabBar("MyTabBar", tab_bar_flags))
+                    {
+                        if (ImGui.BeginTabItem("Avocado"))
+                        {
+                            ImGui.Text("This is the Avocado tab!\nblah blah blah blah blah");
+                            ImGui.EndTabItem();
+                        }
+                        if (ImGui.BeginTabItem("Broccoli"))
+                        {
+                            ImGui.Text("This is the Broccoli tab!\nblah blah blah blah blah");
+                            ImGui.EndTabItem();
+                        }
+                        if (ImGui.BeginTabItem("Cucumber"))
+                        {
+                            ImGui.Text("This is the Cucumber tab!\nblah blah blah blah blah");
+                            ImGui.EndTabItem();
+                        }
+                        ImGui.EndTabBar();
+                    }
+                    ImGui.Separator();
+                    ImGui.TreePop();
+                }
+
+                ImGui.TreePop();
+            }
+        }
+    }
+}
